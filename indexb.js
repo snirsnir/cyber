d0150418a855de4737f10ecea960be92fcab42fc
snir <papasnir@gmail.com>
2019-06-23 21:55:28 +0300

    NOW CAN UPLOAD LESSONS
    READY

--- indexb.js
+++ indexb.js
@@ -20,25 +20,40 @@ function nextScreen()
 			if (clicks == 1){
 			$('#backb').show();
 		}
-        clicks += 1;
+        clicks ++;
         document.getElementById("counter").innerHTML = clicks;
 	var whathide = document.getElementById("hesberD");
 	var flagDorQ; // 0 IS DESCRIPTION SCREEN 1 IS QUEST SCREEN
-	
-	 if (window.getComputedStyle(whathide).display === "none") {
-    alert(rb[3]);
+	//------------------------------------------to the else!!//
+	var title = document.getElementById("mytitle").value
+	var desc = document.getElementById("writequest").value
+    var e = document.getElementById("formchoice");
+    var formaction = e.options[e.selectedIndex].value;
+	var firebaseRef = firebase.database().ref('lessons/type').child(formaction).child(clicks-1);
+	//------------------------------------------to the else!!//
+		//------------------------------------------to the if!!//
+	var correct = document.getElementById("correct").value
+	var questtitle = document.getElementById("mysquest").value
+	var firebaseRef2 = firebase.database().ref('lessons/type').child(formaction).child(clicks-1).child('answers');
+	//------------------------------------------to if!!//
+	 if (window.getComputedStyle(whathide).display === "none") { //if hesber is hidden
+		 flagDorQ = 1;
+		 firebaseRef.child("DorQ").set(flagDorQ);
+		 firebaseRef.child("title").set(questtitle);
+		 firebaseRef.child("correct").set(correct);
+		 var i;
+    for (i=0;i<rb.length;i++){
+		firebaseRef2.child(i).set(rb[i]);
+	}
+		 $('#approve').empty();
+		 counter = 0;
+		 rb = [];
+		 document.getElementById("correct").value= "";
+		 document.getElementById("mysquest").value= "";
+		 document.getElementById("input").value= "";
   }
 	else{
 	flagDorQ = 0;
-	var title = document.getElementById("mytitle").value
-	var desc = document.getElementById("writequest").value
-//Get the select select list and store in a variable
-    var e = document.getElementById("formchoice");
-	
-
-//Get the selected value of the select list
-    var formaction = e.options[e.selectedIndex].value;
-	var firebaseRef = firebase.database().ref('lessons/type').child(formaction).child(clicks-1);
 	firebaseRef.child("title").set(title);
 	firebaseRef.child("desc").set(desc);
 	firebaseRef.child("DorQ").set(flagDorQ);
@@ -63,10 +78,18 @@ function changedSelected()
 function addingRadio()
 {
 	var mybr = document.createElement('br');
-	var name = $('#addingradio').val();
-	$('#approve').append('<input type="radio" />'+name);
+	var name = $('#input').val();
+	$('#approve').append('<input type="radio" { display: none } />'+name);
 	$('#approve').append(mybr);
 	rb[counter] = name;
 	counter++;
 	document.getElementById("addingradio").value= "";
-}       
+
+}
+function chooseScreen(){
+	var temp = clicks;
+	var screen = document.getElementById("screen").value
+	document.getElementById("counter").innerHTML = screen;
+	clicks = screen;
+	
+}
