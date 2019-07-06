7fd6a4f3ba9ace3476e0eb9d7b337745b645f598
snirsnir <YOU@EXAMPLE.COM>
2019-07-06 17:21:37 +0300

    finished front

--- frontjs.js
+++ frontjs.js
@@ -6,11 +6,15 @@ var answersofhesber = document.getElementById("answershesber");
 var screen = 1;
 var whatlesson = addles(screen);
 var countChild;//for counting how much screens in lesson
-var DorQ = "DorQ" //check if hesber or sheelon
+var DorQ = "DorQ"; //check if hesber or sheelon
 var answerscount; //for counthing how much answers
 var i = 1; // I IS THE COUNTER OF FIREBASE starts from 1
 var j = 0; // J IS THE COUNTER OF ANSWERS IN SHELON starts from 0
 var br = document.createElement("br");
+var counterans=1 //counting the answers for names!
+var checkans = document.getElementsByName("answers");
+var fireScreen;
+var getchecked;
 	document.getElementById("adminheader").innerHTML = "שיעור מספר" + " " + screen; //FOR THE TITLE
 	firebaseHeadingRef = firebase.database().ref().child('lessons/type/').child(whatlesson);
 firebaseHeadingRef.once('value', function(snapshot) {
@@ -27,11 +31,13 @@ i++;
 answersofhesber.innerText = "";
   document.body.scrollTop = 0;//TOP OF PAGE
   document.documentElement.scrollTop = 0;
+counterans = 1 //resetting the counterans for next shelon
 doagain();
 }
 function doagain()// DO AGAIN DOING THE FUNCTION AT THE FIRST TIME AND CALLED FROM NEXT SCREEN BUTTON
 {
-	var fireScreen = firebase.database().ref().child('lessons/type/').child(whatlesson).child(i);
+	var outputans="";
+	fireScreen = firebase.database().ref().child('lessons/type/').child(whatlesson).child(i);
 	fireScreen.once('value', function(snapshot) {
   if (snapshot.child(DorQ).val() == "0") { //CHECK IF HESBER OR SHEELON 0 HESBER 1 SHEELON IF 0 TRUE
 	  $('#shelonD2').hide();
@@ -51,10 +57,10 @@ function doagain()// DO AGAIN DOING THE FUNCTION AT THE FIRST TIME AND CALLED FR
 	    answerscount = snapshot.numChildren(); 
 });
 		for (var j=0;j<answerscount;j++){
-			answersofhesber.innerText += snapshot.child('answers/'+j).val()
-			answersofhesber.appendChild(br);
-		}
-			
+			outputans+= '<input type="radio" class="stam" value='+ counterans +' name="answers" onclick=getnumber(value) >' + snapshot.child('answers/'+j).val() + '<br><br>';
+			document.getElementById("answershesber").innerHTML=outputans;
+			counterans++;	
+		}		
 	})
 		}
 		if(i>1){//now the countchild can be counted!
@@ -68,6 +74,22 @@ function doagain()// DO AGAIN DOING THE FUNCTION AT THE FIRST TIME AND CALLED FR
 		}
 });
 }
+function shelonans(){//onclick GUESS BUTTON
+			fireScreen.once('value', function(snapshot) {
+	    var correct = snapshot.child('correct').val();
+				if(getchecked == correct){
+					alert("great");
+				}
+				else{
+					alert("bad");
+				}
+				   
+});
+}
+function getnumber(value){
+		getchecked = value;
+}
+
 
 
 
